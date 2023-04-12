89a2740d9db614a290475d8e5bf0e22a7be79131
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-12 19:29:42 +0300

    up

--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const fieldFindCountry = document.querySelector('#search-box');
+const listСountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
+
 fieldFindCountry.addEventListener(
   'input',
   debounce(() => {
+    fetchCountries()
+      .then(countries => {
+        if (countries.length > 1 && countries.length < 11) {
+          countries => renderListCountry(countries);
+        }
+        countries => renderinfoCountry(countries);
+      })
+
+      .catch(error => console.log(error));
     Notiflix.Notify.success('Scroll handler call after 300ms pause');
   }, DEBOUNCE_DELAY)
 );
@@ -23,8 +35,10 @@ Notiflix.Notify.info('Cogito ergo sum');
 // https://restcountries.eu/rest/v2/{service}?fields={field};{field};{field}
 // https://restcountries.eu/rest/v2/all?fields=name;capital;currencies
 
+// 'https://restcountries.eu/rest/v2/all?fields=name.official;capital;population;flags.svg;languages'
+
 fetchCountries(
-  'https://restcountries.eu/rest/v2/all?fields=name.official;capital;population;flags.svg;languages'
+  'https://restcountries.com/v3.1/all?fields=name,capital,currencies'
 )
   .then(response => {
     if (!response.ok) {
@@ -39,3 +53,7 @@ fetchCountries(
   .catch(error => {
     // Error handling
   });
+
+function renderListCountry(countries) {}
+
+function renderinfoCountry(countries) {}
