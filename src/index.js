1d52022062f905ee836658ce62f7fe7d4bf05ec0
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-04-14 14:26:57 +0300

    debounce

--- src/index.js
+++ src/index.js
@@ -7,21 +7,28 @@ const DEBOUNCE_DELAY = 300;
 const fieldFindCountry = document.querySelector('#search-box');
 const listСountry = document.querySelector('.country-list');
 const infoCountry = document.querySelector('.country-info');
-
+let elementFind = '';
 fieldFindCountry.addEventListener(
   'input',
-  debounce(el => {
-    const elementFind = el.data;
-    // console.log(elementFind);
-    // import fetchCountries from './fetchCountries.js';
-    fetchCountries(elementFind)
-      .then(countries => {
-        renderinfoCountry(countries);
-      })
-
-      .catch(error => console.log(error));
-    Notiflix.Notify.success('Scroll handler call after 300ms pause');
-  }, DEBOUNCE_DELAY)
+  debounce(
+    event => {
+      let elementFind = event.target.value;
+      console.log(event);
+      console.dir(elementFind);
+      // import fetchCountries from './fetchCountries.js';
+      fetchCountries(elementFind)
+        .then(renderinfoCountry)
+        .catch(error => console.log(error));
+      Notiflix.Notify.success(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    },
+    DEBOUNCE_DELAY
+    // {
+    //   leading: true,
+    //   trailing: true,
+    // }
+  )
 );
 
 // Notiflix.Notify.success('Sol lucet omnibus');
@@ -29,18 +36,18 @@ fieldFindCountry.addEventListener(
 // Notiflix.Notify.warning('Memento te hominem esse');
 // Notiflix.Notify.info('Cogito ergo sum');
 
-function fetchCountries(elemInput) {
-  console.log(elemInput);
-  return fetch('https://restcountries.com/v3.1/name/${elemInput}').then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+function fetchCountries(name) {
+  console.log(name);
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name;capital;currencies;population;flags.svg;languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
-  );
+    return response.json();
+  });
 }
-// function renderListCountry(countries) {}
+// function renderCountrybetweenTwoAndTen(countries) {}
 
 // 'https://restcountries.com/v3.1/all?fields=name;capital;currencies;population;flags.svg;languages'
 function renderinfoCountry(countries) {
